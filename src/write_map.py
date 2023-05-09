81a5a5582d8ddaed32721b441d27a78bbf2c3df9
hoshi23 <star.tokyo0310@gmail.com>
2023-05-09 11:59:05 +0900

    map表示の変更

--- src/write_map.py
+++ src/write_map.py
@@ -4,11 +4,12 @@ import argparse
 import pandas as pd
 from count_taste import TASTE
 
-TASTE_COUNT = { TASTE[i] :i for i in range(len(TASTE))}
+TASTE_COUNT = {TASTE[i]: i for i in range(len(TASTE))}
 
-COLORS = ["red", "blue", "green", "purple", "orange", "darkred", "lightred", "beige", 
-         "darkblue", "darkgreen", "cadetblue", "darkpurple", "white", "pink", 
-         "lightblue", "lightgreen", "gray", "black", "lightgray"]
+COLORS = ["red", "blue", "green", "purple", "orange", "darkred", "lightred", "beige",
+          "darkblue", "darkgreen", "cadetblue", "darkpurple", "white", "pink",
+          "lightblue", "lightgreen", "gray", "black", "lightgray"]
+
 
 def get_taste_color(data: Any):
     taste_count = [[data[taste], taste] for taste in TASTE]
@@ -18,26 +19,23 @@ def get_taste_color(data: Any):
     return taste, color
 
 
-
 def main(args):
     print(f"read file: {args.file}")
-    folium_map = folium.Map(location=[35.690921, 139.700258], zoom_start=15)
+    folium_map = folium.Map(location=[35.690921, 139.700258], zoom_start=15,
+                            tiles="cartodbpositron")
     df = pd.read_csv(args.file)
     for _, data in df.iterrows():
         location = [data["latitude"], data["longitude"]]
         shop_name = data["name"]
         taste, color = get_taste_color(data)
-        folium.Marker(
+        folium.CircleMarker(
             location=location,
+            radius=5,
             popup=f"{shop_name}\n taste: {taste}",
-            icon=folium.Icon(color=color, icon='glyphicon')
+            color=color,
+            fill_color=color,
         ).add_to(folium_map)
     folium_map.save('../map/map.html')
-
-
-
-
-
 
 
 if __name__ == "__main__":
